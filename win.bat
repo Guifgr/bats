57f743d22996d7f07f313cce79b6684f6aaabc4c
Guilherme FGR <guifgr@hotmail.com>
2019-10-04 11:51:11 -0700

    update

--- win.bat
+++ win.bat
@@ -8,15 +8,12 @@ cinst googlechrome -fy
 cinst firefox -fy
 cinst vscode -fy
 cinst winrar -fy
-cinst nodejs.install -fy
-cinst python3 -fy
 cinst steam -fy
 cinst brackets -fy
 cinst hyper -fy
 cinst github-desktop -fy
 cinst epicgameslauncher -fy
 cinst protonvpn -fy
-cinst bitnami-xampp -fy
 cinst whatsapp -fy
 cint javaruntime -fy
 cinst discord -fy
@@ -24,6 +21,9 @@ cinst adobe-creative-cloud -fy
 cinst intel-dsa -fy	
 cinst telegram -fy
 cinst git -fy
+cinst bitnami-xampp -fy
+cinst nodejs.install -fy
+cinst python3 -fy
 
 :: cinst OR choco install <package_name> repeats for all the packages you want to install
 
