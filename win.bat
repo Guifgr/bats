6468327f95f06bd1ef7724d19be08eca9c0fd11c
Guilherme FGR <39571461+Guifgr@users.noreply.github.com>
2019-10-04 13:16:12 -0300

    04/10/2019

---
+++ win.bat
@@ -0,0 +1,30 @@
+:: Chocolatey install script
+
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+:: -fy (Force) and confirm (Yes) for any prompt during the install process
+
+cinst googlechrome -fy
+cinst firefox -fy
+cinst vscode -fy
+cinst winrar -fy
+cinst nodejs.install
+cinst python3 -fy
+cinst steam -fy
+cinst brackets -fy
+cinst hyper -fy
+cinst github-desktop -fy
+cinst epicgameslauncher -fy
+cinst protonvpn -fy
+cinst bitnami-xampp -fy
+cinst whatsapp -fy
+cint javaruntime -fy
+cinst discord -fy
+cinst adobe-creative-cloud -fy
+cinst intel-dsa -fy	
+cinst telegram -fy
+cinst git -fy
+
+:: cinst OR choco install <package_name> repeats for all the packages you want to install
+
+exit
